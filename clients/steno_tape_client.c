14bbd06b6afbe3ffb8d7778851703f7ee50c284a
Paul Fioravanti <paul.fioravanti@gmail.com>
2023-08-17 11:40:06 +1000

    Refactor client to use integer arguments and a switch statement

--- clients/steno_tape_client.c
+++ clients/steno_tape_client.c
@@ -1,24 +1,34 @@
-#include <string.h>        // strcmp
-#include <steno_tape.h>    // steno_tape_cleanup, steno_tape_init,
-                           // steno_tape_log_entry, steno_tape_log_gaming_mode,
-                           // steno_tape_log_steno_mode, Tape
+#include <errno.h>      // errno
+#include <limits.h>     // INT_MAX, INT_MIN
+#include <stdio.h>      // printf
+#include <stdlib.h>     // strtol
+#include <string.h>     // strlen
+#include <steno_tape.h> // steno_tape_cleanup, steno_tape_init,
+                        // steno_tape_log_entry, steno_tape_log_gaming_mode,
+                        // steno_tape_log_steno_mode, Tape
 
-static const char * const CUSTOM_ACTIONS[] = {
-  "DASH_BACKWARD",
-  "DASH_FORWARD",
-  "DASH_LEFT",
-  "DASH_RIGHT"
+enum {
+  SWITCH_TO_STENO_MODE = 0,
+  SWITCH_TO_GAMING_MODE = 1,
+  DASH_FORWARD = 2,
+  DASH_BACKWARD = 3,
+  DASH_LEFT = 4,
+  DASH_RIGHT = 5
 };
-static const char * const CUSTOM_ACTION_TAPE_ENTRIES[] = {
-  "COMBO |     W        R B G S  | W-RBGS (D,J,K,L,;) -> {#CONTROL(BACKSPACE)}{^/db}{#RETURN}\n",
-  "COMBO |    P         R B G S  | P-RBGS (E,J,K,L,;) -> {#CONTROL(BACKSPACE)}{^/df}{#RETURN}\n",
-  "COMBO |   K          R B G S  | K-RBGS (S,J,K,L,;) -> {#CONTROL(BACKSPACE)}{^/dl}{#RETURN}\n",
-  "COMBO |       R      R B G S  | R-RBGS (F,J,K,L,;) -> {#CONTROL(BACKSPACE)}{^/dr}{#RETURN}\n"
-};
-static const int NUM_CUSTOM_ACTIONS =
-  sizeof(CUSTOM_ACTIONS) / sizeof(CUSTOM_ACTIONS[0]);
+
+static const char DASH_FORWARD_ENTRY[]  = "COMBO |    P         R B G S  | P-RBGS (E,J,K,L,;) -> {#CONTROL(BACKSPACE)}{^/df}{#RETURN}\n";
+static const char DASH_BACKWARD_ENTRY[] = "COMBO |     W        R B G S  | W-RBGS (D,J,K,L,;) -> {#CONTROL(BACKSPACE)}{^/db}{#RETURN}\n";
+static const char DASH_LEFT_ENTRY[]     = "COMBO |   K          R B G S  | K-RBGS (S,J,K,L,;) -> {#CONTROL(BACKSPACE)}{^/dl}{#RETURN}\n";
+static const char DASH_RIGHT_ENTRY[]    = "COMBO |       R      R B G S  | R-RBGS (F,J,K,L,;) -> {#CONTROL(BACKSPACE)}{^/dr}{#RETURN}\n";
+
+static int parse_arguments(int argc, char *argv[]);
 
 int main(int argc, char *argv[]) {
+  int arg = parse_arguments(argc, argv);
+  if (arg == -1) {
+    return -1;
+  }
+
   Tape *tape = steno_tape_init();
   if (!tape) {
     printf("ERROR: Unable to open log file\n");
@@ -26,30 +36,56 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
-  const char *action = argv[1];
-  int known_action = 0;
-
-  if (strcmp(action, "GAMING_MODE") == 0) {
-    known_action = 1;
-    steno_tape_log_gaming_mode(tape);
-  } else if (strcmp(action, "STENO_MODE") == 0) {
-    known_action = 1;
-    steno_tape_log_steno_mode(tape);
-  } else {
-    for (int i = 0; i < NUM_CUSTOM_ACTIONS; i++) {
-      if (strcmp(action, CUSTOM_ACTIONS[i]) == 0) {
-        known_action = 1;
-        steno_tape_log_entry(tape, CUSTOM_ACTION_TAPE_ENTRIES[i]);
-      }
-    }
-  }
-
-  if (!known_action) {
-    printf("Unknown action: %s\n", action);
-    steno_tape_cleanup(tape);
-    return -1;
+  switch (arg) {
+    case SWITCH_TO_STENO_MODE:
+      steno_tape_log_steno_mode(tape);
+      break;
+    case SWITCH_TO_GAMING_MODE:
+      steno_tape_log_gaming_mode(tape);
+      break;
+    case DASH_FORWARD:
+      steno_tape_log_entry(tape, DASH_FORWARD_ENTRY);
+      break;
+    case DASH_BACKWARD:
+      steno_tape_log_entry(tape, DASH_BACKWARD_ENTRY);
+      break;
+    case DASH_LEFT:
+      steno_tape_log_entry(tape, DASH_LEFT_ENTRY);
+      break;
+    case DASH_RIGHT:
+      steno_tape_log_entry(tape, DASH_RIGHT_ENTRY);
+      break;
+    default:
+      printf("Unknown argument: %d\n", arg);
+      steno_tape_cleanup(tape);
+      return -1;
   }
 
   steno_tape_cleanup(tape);
   return 0;
+}
+
+static int parse_arguments(int argc, char *argv[]) {
+  // Requires only one argument
+  // REF: https://stackoverflow.com/questions/9748393/how-can-i-get-argv-as-int
+  if (argc != 2 || strlen(argv[1]) == 0) {
+    printf("ERROR: Must provide a single non-empty argument\n");
+    return -1;
+  }
+
+  char *end_ptr;
+  errno = 0;
+  // REF: https://devdocs.io/c/string/byte/strtol
+  int arg = strtol(argv[1], &end_ptr, 16);
+
+  // Error out if:
+  // - an invalid character was found before the end of the string
+  // - overflow or underflow errors occurred
+  // - number is outside the limited capacity of an int
+  if (*end_ptr != '\0' || errno != 0 || arg < INT_MIN || arg > INT_MAX) {
+    printf("ERROR: Invalid argument %s\n", argv[1]);
+    return -1;
+  }
+
+  return arg;
 }
